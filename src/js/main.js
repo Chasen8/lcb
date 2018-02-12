e713ec810fc603ac232f25930e5285c190db65b5
Chasen8 <361315043@qq.com>
2018-02-12 15:45:20 +0800

    test

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,14 @@
 * @Author: Pearl8
 * @Date:   2018-02-10 11:42:50
 * @Last Modified by:   Pearl8
-* @Last Modified time: 2018-02-12 14:28:58
+* @Last Modified time: 2018-02-12 15:42:42
 */
 
 'use strict';
 /*
  *  Default theme - Owl Carousel CSS File
  */
-;$(document).ready(function () {
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
