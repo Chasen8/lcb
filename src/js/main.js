b869ad2624f264f9a5dd48f9ea16afe11e646670
Chasen8 <361315043@qq.com>
2018-02-12 15:17:06 +0800

    end

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,19 @@
 * @Author: Pearl8
 * @Date:   2018-02-10 11:42:50
 * @Last Modified by:   Pearl8
-* @Last Modified time: 2018-02-10 11:47:27
+* @Last Modified time: 2018-02-12 14:28:58
 */
 
 'use strict';
+/*
+ *  Default theme - Owl Carousel CSS File
+ */
+;$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
--- src/js/modernizr.js
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: Pearl8
-* @Date:   2018-02-10 11:43:30
-* @Last Modified by:   Pearl8
-* @Last Modified time: 2018-02-10 11:47:22
-*/
-
-'use strict';
